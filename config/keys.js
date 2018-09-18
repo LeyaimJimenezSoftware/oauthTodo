330e169e871cb5fbbd4ceef6bab20e2a1be1ad30
LeyaimJimenezSoftware <ljimenez6@ucol.mx>
2018-09-18 08:46:05 -0500

    finished auth 2.0

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 // keys.js figure out what set credencials too return
-if(proccess.env.NODE_ENV === 'production'){
-        // we re in production return prod seet keys
-        module.exports = require('./prod')
-}else{
-        // we re in development -return dev keys
-        module.exports = require('./dev')
+if (process.env.NODE_ENV === 'production') {
+  // we re in production return prod seet keys
+  module.exports = require('./prod')
+} else {
+  // we re in development -return dev keys
+  module.exports = require('./dev')
 }
